98012c6beb7c647eb28fcd45fe3547a8d729dbc4
CoronetLiu <865753224@qq.com>
2018-02-08 10:00:07 +0800

    First Commit

---
+++ javascripts/index.js
@@ -0,0 +1,9 @@
+/*
+* @Author:  CoronetLiu
+* @Date:    2018-02-08 09:30:08
+* @Last Modified by:    CoronetLiu
+* @Last Modified time:  2018-02-08 09:30:10
+* @Email:   liu86931@163.com
+*/
+
+// 'use strict';
