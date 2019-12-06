1138d82a05b3b2cea89ec436ea70db1380dc9a13
CoronetLiu <865753224@qq.com>
2019-12-06 18:20:43 +0800

    dqy

--- javascripts/dqy.js
+++ javascripts/dqy.js
@@ -3,3 +3,17 @@
  * 2019/12/6
  */
 
+let rd = 0;
+setInterval(function() {
+  rd > 3 ? (rd = 0) : (rd += 1);
+  $('#dqy').css({
+    transform: 'rotateX(35deg) rotateY(' + (rd * 90 - 45) + 'deg) scaleZ(1)',
+  });
+  for (let i = 0; i < $('.outer').length; i++) {
+    if ($('.outer')[i].className.indexOf('active') !== -1) {
+      $($('.outer')[i]).removeClass('active');
+    } else {
+      $($('.outer')[i]).addClass('active');
+    }
+  }
+}, 2000);
