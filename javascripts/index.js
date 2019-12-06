e38c09922138054a2388cd96282bc95c10ab2d53
CoronetLiu <865753224@qq.com>
2019-12-06 14:56:22 +0800

    Home

--- javascripts/index.js
+++ javascripts/index.js
@@ -2,25 +2,17 @@
 * Author: CoronetLiu   2018/6/12
 */
 
-// 'use strict';
-
-
-$(function(){
-    $("#control").on("click",function(){
-        // alert(1)
-        var audio = $("#audio")[0];
-        if($(this).attr("data-id") == "on"){
-            $(this).attr("data-id","off")
-            this.style.backgroundImage = "url('./resource/images/pause.png')"
-            audio.pause();
-        }else{
-            $(this).attr("data-id","on")
-            this.style.backgroundImage = "url('./resource/images/play.png')"
-            audio.play();
-        }
-    })
-
-
-
-
-})//$function
+$(function() {
+  $('#control').on('click', function() {
+    var audio = $('#audio')[0];
+    if ($(this).attr('data-id') === 'on') {
+      $(this).attr('data-id', 'off');
+      this.style.backgroundImage = 'url(\'./resource/images/pause.png\')';
+      audio.pause();
+    } else {
+      $(this).attr('data-id', 'on');
+      this.style.backgroundImage = 'url(\'./resource/images/play.png\')';
+      audio.play();
+    }
+  });
+});//$function
