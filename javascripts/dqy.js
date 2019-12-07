e20bc125022c3884da2fc38469e20adfbaf9eed8
CoronetLiu <865753224@qq.com>
2019-12-08 00:18:56 +0800

    dqy

--- javascripts/dqy.js
+++ javascripts/dqy.js
@@ -7,7 +7,7 @@ let rd = 0;
 setInterval(function() {
   rd > 3 ? (rd = 0) : (rd += 1);
   $('#dqy').css({
-    transform: 'rotateX(35deg) rotateY(' + (rd * 90 - 45) + 'deg) scaleZ(1)',
+    transform: 'rotateX(-35deg) rotateY(' + (rd * 90 - 45) + 'deg) scaleZ(1)',
   });
   for (let i = 0; i < $('.outer').length; i++) {
     if ($('.outer')[i].className.indexOf('active') !== -1) {
